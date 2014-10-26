161f0a48065fd9066e7bd4e5b1949c20ac3ecf34
somtomking <somtomking@163.com>
2014-10-26 22:52:44 +0800

    文件添加，第一次

---
+++ Src/Freedom.Web/Freedom.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Freedom.Web.MvcApplication" Language="C#" %>
---
+++ Src/Freedom.Web/Freedom/Domain/Customers/SystemAccountNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freedom.Domain.Customers
+{
+    public static class SystemAccountNames
+    {
+        public readonly static string SearchEngine = "SearchEngine";
+        public readonly static string BackgroundTask = "BackgroundTask";
+    }
+}
