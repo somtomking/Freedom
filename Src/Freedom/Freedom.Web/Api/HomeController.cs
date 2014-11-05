c49c36562900a79f9728fab3695f888699d48fe8
somtomking <somtomking@163.com>
2014-11-06 04:25:32 +0800

    api

---
+++ Src/Freedom/Freedom.Web/Api/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Freedom.Web.Api
+{
+    public class HomeController : ApiController
+    {
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}
